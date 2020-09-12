257f6fef6d490cb6146cfdf3978674fb16209f91
Jayden <dshung1997@gmail.com>
2020-09-12 08:53:27 +0700

    refactor(remove-ads): throttle callback and remove feed units on the fly

--- remove_ads.js
+++ remove_ads.js
@@ -1,43 +1,72 @@
 // ==UserScript==
 // @name         Hide sponsored items
-// @namespace    Jayden
+// @namespace    whatisthis
 // @version      0.1
 // @description  try to sanitize Facebook
-// @author       Jayden
+// @author       Sy Hung Doan
 // @match        https://www.facebook.com/*
 // @match        https://facebook.com/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  // TODO: remove only when the number of items increases
-
-  const pattern =
-    "//*[@aria-label='Sponsored']//ancestor::div[contains(@data-pagelet,'FeedUnit_')]";
-  const itemsContainer = document.querySelector("div[role='feed']");
-
-  const config = { childList: true };
-
-  const callback = () => {
-    const items = document.evaluate(
-      pattern,
-      document,
-      null,
-      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-      null
-    );
-    const length = items.snapshotLength;
-    if (length) {
-      console.log(`Found ${length} ads`);
-    }
-
-    for (let i = 0; i < length; i++) {
-      const item = items.snapshotItem(i);
-      itemsContainer.removeChild(item);
-      // console.log('Remove ', i);
-    }
+  // src: https://codeburst.io/throttling-and-debouncing-in-javascript-b01cad5c8edf
+  const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan;
+    return function () {
+      const context = this;
+      const args = arguments;
+      if (!lastRan) {
+        func.apply(context, args);
+        lastRan = Date.now();
+      } else {
+        clearTimeout(lastFunc);
+        lastFunc = setTimeout(function () {
+          if (Date.now() - lastRan >= limit) {
+            func.apply(context, args);
+            lastRan = Date.now();
+          }
+        }, limit - (Date.now() - lastRan));
+      }
+    };
   };
 
-  const observer = new MutationObserver(callback);
-  observer.observe(itemsContainer, config);
+  let lastCommitIndex = 0;
+  let unitsContainer = document.querySelector("div[role='feed']");
+
+  const callback = () => {
+    unitsContainer = document.querySelector("div[role='feed']");
+    if (!unitsContainer) {
+      return;
+    }
+
+    const feedUnits = Array.from(
+      document.querySelectorAll(
+        `div[role='feed'] > div[data-pagelet^='FeedUnit_']:nth-child(n+${lastCommitIndex})`
+      )
+    );
+
+    if (!feedUnits.length) {
+      return;
+    }
+
+    Array(feedUnits.length)
+      .fill(0)
+      .forEach((_, i, obj) => {
+        const unit = feedUnits[i];
+
+        if (unit.querySelector("[aria-label='Sponsored']")) {
+          unitsContainer.removeChild(unit);
+          obj.splice(i, 1);
+        } else {
+          lastCommitIndex = i;
+        }
+      });
+  };
+
+  const callbackThrottled = throttle(callback, 1000);
+
+  const observer = new MutationObserver(callbackThrottled);
+  observer.observe(unitsContainer, { childList: true });
 })();
